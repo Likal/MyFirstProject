5187f6dc9e65d98c5584d172c165d9d7ce8e3c61
Lisa K <lkalych@gmail.com>
2020-06-29 16:45:00 -0600

    my first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Lisa";
+            Console.WriteLine("Hello " + myName);
+
+            
+        }
+    }
+}
