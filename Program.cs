1091a8704ddb88b93da1625c075767f52d369423
Lisa K <lkalych@gmail.com>
2020-07-20 13:04:55 -0600

    comment practice

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace MyFirstProject
             Console.WriteLine("How old are you?");
       int age = Convert.ToInt32(Console.ReadLine());
       
-
+        //Practicing entering a comment
       Console.WriteLine("You are {0} years old", age);
         }
     }
