cb50da941011d2c0155169ed8d13c0251a66c076
Lisa K <lkalych@gmail.com>
2020-07-21 17:18:23 -0600

    two if statements nested and else

--- Program.cs
+++ Program.cs
@@ -6,15 +6,19 @@ namespace MyFirstProject
     {
      static void Main(string[] args)
      {
-       int mark = 85;
+       int mark = 100;
 
-       if (mark < 50) //if is if its true
+       if (mark >= 50) //if is if its true
        {
-         Console.WriteLine("You failed.");
+         Console.WriteLine("You passed.");
+         if(mark == 100)
+         {
+           Console.WriteLine("Perfect!");
+         }
        }
        else //Else is if its false.
        {
-         Console.WriteLine("You passed.");
+         Console.WriteLine("You failed.");
        }
      }
         
