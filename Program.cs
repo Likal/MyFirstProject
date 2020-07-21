917504fc3027815b9bd8b48bfb25d607e9d7da80
Lisa K <lkalych@gmail.com>
2020-07-21 16:41:12 -0600

    if statement

--- Program.cs
+++ Program.cs
@@ -4,13 +4,16 @@ namespace MyFirstProject
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("How old are you?");
-      int age = Convert.ToInt32(Console.ReadLine());
-      
-        //Practicing entering a comment
-      Console.WriteLine("You are {0} years old", age);
-        }
+     static void Main(string[] args)
+     {
+       int x = 8;
+       int y = 3;
+
+       if (x > y)
+       {
+         Console.WriteLine("x is greater than y");
+       }
+     }
+        
     }
 }
