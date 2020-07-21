99486426bab890f2331dd01a62845cf51003fbae
Lisa K <lkalych@gmail.com>
2020-07-21 16:52:12 -0600

    comment added

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace MyFirstProject
        int x = 8;
        int y = 3;
 
+        //you can omit the curly braces if there is only one block of code if you wish.
+        //if (x > y)
+        //Console.WriteLine("x is greater than y");
+
        if (x > y)
        {
          Console.WriteLine("x is greater than y");
